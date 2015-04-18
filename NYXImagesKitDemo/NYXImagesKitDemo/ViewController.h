a4729fb012465f91e052fe56a94581f737367429
Michele <michele.titolo@gmail.com>
2015-04-18 13:50:43 -0700

    Reorganize files and add FBSnapshotTestCase pod to demo

---
+++ NYXImagesKitDemo/NYXImagesKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NYXImagesKitDemo
+//
+//  Created by Michele Titolo on 4/18/15.
+//  Copyright (c) 2015 MacGeneration. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NYXImagesKitDemo/NYXImagesKitDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NYXImagesKitDemo
+//
+//  Created by Michele Titolo on 4/18/15.
+//  Copyright (c) 2015 MacGeneration. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
